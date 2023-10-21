5a53c9c34466e62c55ec9c44a821f3f4497aa2ef
Myllena <mssilva.egq@uesc.br>
2023-10-21 20:24:34 -0300

    adicionando class consulta e main

--- clinica.cpp
+++ clinica.cpp
@@ -77,3 +77,76 @@ class Medico{
     }
 
 };
+
+class Consulta{
+    private:
+
+        char efetuada;
+        int tempConsulta;
+        string convenio;
+        Paciente * paciente;
+        Medico * medico;
+
+    public:
+        Consulta(){
+            this->efetuada = 'n';
+            this->convenio = "nenhum";
+        }
+        char getEfetuada(){
+            return this->efetuada;
+        }
+        void setEfetuada(char _efetuada){
+            this->efetuada = _efetuada;
+        }
+        int getTempConsulta(){
+            return this->tempConsulta;
+        }
+        void setTempConsulta(int _tempConsulta){
+            this->tempConsulta = _tempConsulta;
+        }
+        string getConvenio(){
+            return this->convenio;
+        }
+        void setConvenio(string _convenio){
+            this->convenio = _convenio;
+        }
+        Paciente * getPaciente(){
+            return this->paciente;
+        }
+        void setPaciente(Paciente * _paciente){
+            this->paciente = _paciente;
+        }
+        Medico * getMedico(){
+            return this->medico;
+        }
+        void setMedico(Medico * _medico){
+            this->medico = _medico;
+        }
+};
+
+int main (){
+    vector<Paciente *> pacientes;
+    vector<Medico *> medicos;
+
+    int opcao;
+    do{
+        cout << "_____________MENU_____________" << endl;
+        cout << "1. Gestão de Pacientes" << endl;
+        cout << "2. Gestão de Medicos" << endl;
+        cout << "3. Gestão de Consultas" << endl;
+        cout << "0. Sair" << endl;
+        cin >> opcao;
+
+        switch (opcao){
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+        }
+    }while (opcao != 0);
+    return 0;
+}
